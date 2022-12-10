559ac7e9cea8131c899f2e8bf9205855c5b4f657
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-12-10 23:35:00 +0100

    Keep prompt user number between 1 and 10

---
+++ snack4/script.js
@@ -0,0 +1,7 @@
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+let userNumber;
+
+do {
+    userNumber = parseInt(prompt('Inserisci un numero da 1 a 10:'), 10);
+} while ((userNumber < 1) || (userNumber > 10));
